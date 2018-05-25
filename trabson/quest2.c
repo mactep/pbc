55720a14bfafed2216f6782674d8fa5acaf38b41
mactepp <tulio@brunoro.org>
2018-05-25 10:51:32 -0300

    Atualizar questão 2

--- trabson/quest2.c
+++ trabson/quest2.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
+void imprime_vetor(int tam, int vet[tam]) {
+  if (vet[0] <= vet[tam - 1]) {
+    printf("O vetor crescente e: ");
+    for (int i = 0; i < tam; i++) {
+      printf("%d ", vet[i]);
+    }
+  } else {
+    printf("O vetor decrescente e: ");
+    for (int i = 0; i < tam; i++) {
+      printf("%d ", vet[i]);
+    }
+  }
+}
+
 int main(int argc, char const *argv[]) {
   int vetNums[4], vetCrescente[4], vetDecrescente[4];
   for (int i = 0; i < 4; i++) {
@@ -7,10 +21,5 @@ int main(int argc, char const *argv[]) {
   }
   // Ordenar os valores aqui. Usar algum dos algoritimos
   int aux;
-  for (int i = 0; i < 4; i++) {
-    if (/* condition */) {
-      /* code */
-    }
-  }
   return 0;
 }
