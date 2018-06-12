3fed64cd6cdc8602276365606fcfe66523ae6026
mactepp <tulio@brunoro.org>
2018-06-12 19:57:20 -0300

    Adiciona checagem de jogada ao trabalho

--- trabalho/Trabalho.c
+++ trabalho/Trabalho.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int **le_matriz_chave(int *tam) {
   FILE *entrada;
@@ -51,28 +52,54 @@ void imprime_jogo(int tam, int **matriz) {
   }
 }
 
-typedef struct {
+/*typedef struct {
   int x, y;
-} posicao;
+} posicao;*/
 
-posicao pega_jogada(int tam) {
-  posicao p;
-  p.x = -1;
-  p.y = -1;
-  while (p.x == -1 || p.x < 0 || p.x >= tam) {
+void pega_jogada(int jogador, int tam, int **campo, int **chave) {
+  //posicao p;
+  int x = -1;
+  int y = -1;
+  while (x == -1 || y == -1) {
     printf("Informe um valor para X: ");
-    scanf("%d", &p.x);
-  }
-  while (p.y == -1 || p.y < 0 || p.y >= tam) {
+    scanf("%d", &x);
+
     printf("Informe um valor para Y: ");
-    scanf("%d", &p.y);
+    scanf("%d", &y);
+
+    if (x < 0 || y < 0 || x >= tam || y >= tam) {
+      printf("Posicao fora do campo. Informe outra jogada\n");
+      x = -1;
+    } else if (jogador == 1 && y < tam/2) {
+      printf("Posicao no proprio campo. Informe outra jogada\n");
+      y = -1;
+    } else if (jogador == -1 && y >= tam/2) {
+      printf("Posicao no proprio campo. Informe outra jogada\n");
+      y = -1;
+    } else if (campo[x][y] == 1) {
+      printf("Posicao ja descoberta. Informe outra jogada\n");
+      x = -1;
+      y = -1;
+    } else {
+      printf("So jogada top, Rogerinho!\n");
+      printf("Posicao informada: (%d, %d)\n", x, y);
+    }
   }
-  return p;
+  if (chave[x][y] == 1) {
+    printf("Acertou mais navio que Kamikaze em Pearl Harbor!\n");
+    campo[x][y] = 1;
+  } else {
+    printf("Errou feio, errou rude!\n");
+  }
+  /*return p;*/
 }
 
 int main(int argc, char const* argv[])
 {
   int tamanho;
+  //jogador_atual vai ser 1(primeiro) e -1(segundo)
+  int jogador_atual = 1;
+  int jogando = 1;
 
   int **chave = le_matriz_chave(&tamanho);
   printf("%d\n", tamanho);
@@ -80,6 +107,13 @@ int main(int argc, char const* argv[])
   int **campo = cria_matriz_vazia(tamanho);
   imprime_jogo(tamanho, campo);
 
-  posicao eoq = pega_jogada(tamanho);
+  while (jogando) {
+    system("clear");
+    printf("Jogando agora: jogador %d\n", jogador_atual);
+    imprime_jogo(tamanho, campo);
+    pega_jogada(jogador_atual, tamanho, campo, chave);
+    sleep(2);
+    jogador_atual *= -1;
+  }
   return 0;
 }
