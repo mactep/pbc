36b587e640824f411a9b63a01ffe3bbdceb89b00
mactepp <tulio@brunoro.org>
2018-06-12 08:56:15 -0300

    Updates no trabalho

--- trabalho/Trabalho.c
+++ trabalho/Trabalho.c
@@ -1,23 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int **cria_matriz_chave() {
+int **le_matriz_chave(int *tam) {
   FILE *entrada;
-  int N;
 
   entrada = fopen("batalhaNaval.txt", "r");
   if (entrada == NULL) {
     printf("Erro ao ler arquivo de entrada\n");
     exit(1);
   }
-  fscanf(entrada, "%d", &N);
-  int **campo = (int **)malloc(N * sizeof(int*));
-  for (int i = 0; i < N; i++) {
-    campo[i] = (int *)malloc(N * sizeof(int));
+  fscanf(entrada, "%d", tam);
+  int **campo = (int **)malloc(*tam * sizeof(int*));
+  for (int i = 0; i < *tam; i++) {
+    campo[i] = (int *)malloc(*tam * sizeof(int));
   }
 
-  for (int i = 0; i < N; i++) {
-    for (int j = 0; j < N; j++) {
+  for (int i = 0; i < *tam; i++) {
+    for (int j = 0; j < *tam; j++) {
       fscanf(entrada, "%d", &campo[i][j]);
     }
   }
@@ -37,19 +36,50 @@ int **cria_matriz_vazia(int tam) {
   return m;
 }
 
-//TODO: melhorar essa funcao
-void imprime_jogo(int tam, int matriz[tam][tam]) {
+void imprime_jogo(int tam, int **matriz) {
   for (int i = 0; i < tam; i++) {
+    if (i == tam/2) {
+      for (int k = 0; k < 2*tam; k++) {
+        printf("-");
+      }
+      printf("\n");
+    }
     for (int j = 0; j < tam; j++) {
       printf("%d ", matriz[i][j]);
     }
     printf("\n");
   }
-  printf("----------\n");
+}
+
+typedef struct {
+  int x, y;
+} posicao;
+
+posicao pega_jogada(int tam) {
+  posicao p;
+  p.x = -1;
+  p.y = -1;
+  while (p.x == -1 || p.x < 0 || p.x >= tam) {
+    printf("Informe um valor para X: ");
+    scanf("%d", &p.x);
+  }
+  while (p.y == -1 || p.y < 0 || p.y >= tam) {
+    printf("Informe um valor para Y: ");
+    scanf("%d", &p.y);
+  }
+  return p;
 }
 
 int main(int argc, char const* argv[])
 {
-  int **campo = cria_matriz_chave();
+  int tamanho;
+
+  int **chave = le_matriz_chave(&tamanho);
+  printf("%d\n", tamanho);
+
+  int **campo = cria_matriz_vazia(tamanho);
+  imprime_jogo(tamanho, campo);
+
+  posicao eoq = pega_jogada(tamanho);
   return 0;
 }
